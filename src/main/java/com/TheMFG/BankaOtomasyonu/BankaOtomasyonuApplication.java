5cfd5aef73037a179217f340b9e493d21f75a090
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-08-29 13:23:28 +0300

    proje oluşturuldu

---
+++ src/main/java/com/TheMFG/BankaOtomasyonu/BankaOtomasyonuApplication.java
@@ -0,0 +1,13 @@
+package com.TheMFG.BankaOtomasyonu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankaOtomasyonuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankaOtomasyonuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TheMFG/BankaOtomasyonu/BankaOtomasyonuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheMFG.BankaOtomasyonu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaOtomasyonuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
